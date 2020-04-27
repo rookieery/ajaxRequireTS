e185a9ba4183f54445711998cf5bcc8fe59d476e
Rookie <Simon.Lu@grapecity.com>
2020-04-27 13:42:41 +0800

    update

--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,6 @@
 // import "amd-loader";
 import { Ajax as AjaxPromise } from './promiseAjax';
-import { Ajax as AjaxAsync} from './asyncAjax';
+import { Ajax as AjaxAsync } from './asyncAjax';
 
 class Main {
   public static start() {
@@ -10,7 +10,7 @@ class Main {
   private static async clickHandler() {
     const url = './package.json';
     const HTTPMethodType = 'GET';
-    const headerMap = new Map();
+    const headerMap = new Map<string, string>();
     headerMap.set('Accept', 'application/json');
     try {
       const result = await AjaxPromise.PromiseAjax.getPromiseJSON(url, HTTPMethodType, headerMap);
